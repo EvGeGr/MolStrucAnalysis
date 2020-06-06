bf37a2194588575f970c198175181240cfa775b6
Evgeniy <egromov@gmx.de>
2020-06-07 00:34:27 +0200

    Mol-class has been added to mol.py.

--- mol.py
+++ mol.py
@@ -13,22 +13,28 @@ class Mol:
                   "Geometry is empty!")
             self.geom=None
 
-        xyz_array = []
-        for line in lines:
-            xyz_array.append(line.split())
+        if lines:
+            xyz_array = []
+            for line in lines:
+                xyz_array.append(line.split())
 
-        try:
-            n_atoms = int(xyz_array[0][0])
-        except(TypeError):
-            print("A number of atoms (integer) is expected in the first line of the geometry file")
+            try:
+                n_atoms = int(xyz_array[0][0])
+            except(TypeError):
+                print("A number of atoms (integer) is expected in the first line of the geometry file")
 
-        at_types=[]
-        coords = numpy.zeros((n_atoms, 3))
-        for i in range(n_atoms):
-            at_types.append(xyz_array[i + 2][0])
-            for j in range(3):
-                coords[i][j] = float(xyz_array[i + 2][j + 1])
-        self.geom = [at_types, coords]
+            at_types=[]
+            coords = numpy.zeros((n_atoms, 3))
+            for i in range(n_atoms):
+                at_types.append(xyz_array[i + 2][0])
+                for j in range(3):
+                    try:
+                        coords[i][j] = float(xyz_array[i + 2][j + 1])
+                    except(TypeError):
+                        print("Error in reading in coordinates:\n"
+                              "x, y, z coordinates are expected for each atom after the atom symbol!\n"
+                              "A coordinate is assigned to '0' if you see this error")
+            self.geom = [at_types, coords]
 
 
 
