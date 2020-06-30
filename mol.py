1a554244456ecdff8728526373eadd7c38884379
Evgeniy <egromov@gmx.de>
2020-07-01 00:04:43 +0200

    Merging 'read_geom and get_bond_graph methods in Mol-class and adding 'print_bond_graph' and 'print_hbond_graph' methods to Mol-class.

--- mol.py
+++ mol.py
@@ -2,11 +2,14 @@ import numpy, functions, const
 
 class Mol:
 
-    #Reading in molecular Cartesians from a file
+    #Reading in molecular Cartesian coordinates from a file
     #First line of the file - number of atoms (n_atoms)
     #Seconf line of the file - comment
     #Each next of n_atoms lines: Atom type (at_type) X-coordinate  Y-coordinate  Z-coordinate
     def read_geom(self, geom_file=None):
+        if self.__geom:
+            print("Geometry has already been read in!\n")
+            return
         self.__geom_file = geom_file
         lines = None
         if geom_file:
@@ -17,8 +20,8 @@ class Mol:
             except (FileNotFoundError, IOError):
                 print("File {} not found".format(self.__geom_file))
         else:
-            print("No geometry file is given!\n"
-                  "Geometry is empty!")
+            print("No geometry file has been provided!")
+            self.read_in_geom_err()
             self.__geom = None
 
         if lines:
@@ -48,51 +51,56 @@ class Mol:
                               "A coordinate is assigned to '0' if you see this error")
             self.__geom = [comment, at_types, coords]
 
-    # build graph of which atoms are covalently bonded
-    def get_bond_graph(self,geom):
-        self.__bond_graph=None
-        comment, at_types, coords = geom[0:3]
-        n_atoms = len(at_types)
-        self.__bond_graph = [[] for i in range(n_atoms)]
-        for i in range(n_atoms):
-            covrad1 = const.cov_rads[at_types[i]]
-            for j in range(i + 1, n_atoms):
-                covrad2 = const.cov_rads[at_types[j]]
-                thresh = const.bond_thresh * (covrad1 + covrad2)
-                r12 = functions.get_r12(coords[i], coords[j])
-                if (r12 < thresh):
-                    self.__bond_graph[i].append(j)
-                    self.__bond_graph[j].append(i)
-        # check for H-bonds
-        nhb = 0
-        for i in range(n_atoms):
-            if at_types[i] in const.hB_atoms:
-                for j in filter(lambda x: x != i, range(n_atoms)):
-                    if at_types[j] == 'H' and i not in self.__bond_graph[j]:
-                        if any(hB_atom in list(at_types[x] for x in self.__bond_graph[j]) for hB_atom in const.hB_atoms):
-                            r12 = functions.get_r12(coords[i], coords[j])
-                            if r12 < const.hB_thresh:
-                                nhb += 1
-                                self.__bond_graph[i].append(j)
-                                self.__bond_graph[j].append(i)
-#        return bond_graph, nhb
+            # building graph of covalently bonded atoms (bond_graph)
+            self.__bond_graph = [[] for i in range(n_atoms)]
+            for i in range(n_atoms):
+                covrad1 = const.cov_rads[at_types[i]]
+                for j in range(i + 1, n_atoms):
+                    covrad2 = const.cov_rads[at_types[j]]
+                    thresh = const.bond_thresh * (covrad1 + covrad2)
+                    r12 = functions.get_r12(coords[i], coords[j])
+                    if (r12 < thresh):
+                        self.__bond_graph[i].append(j)
+                        self.__bond_graph[j].append(i)
 
+            # checking for H-bonds and if there are H-bonds building a graph for them (hbond_graph)
+            # adding H-bonds to bond_graph
+            self.__hbond_graph = [[] for i in range(n_atoms)]
+            for i in range(n_atoms):
+                if at_types[i] in const.hB_atoms:
+                    for j in filter(lambda x: x != i, range(n_atoms)):
+                        if at_types[j] == 'H' and i not in self.__bond_graph[j]:
+                            if any(hB_atom in list(at_types[x] for x in self.__bond_graph[j]) for hB_atom in
+                                   const.hB_atoms):
+                                r12 = functions.get_r12(coords[i], coords[j])
+                                if r12 < const.hB_thresh:
+                                    self.__nhb+=1
+                                    self.__hbond_graph[i].append(j)
+                                    self.__hbond_graph[j].append(i)
+                                    self.__bond_graph[i].append(j)
+                                    self.__bond_graph[j].append(i)
+
+    #Processing error "no geomtry has been read in"
+    def read_in_geom_err(self):
+        print("No geometry has been read in\n\rUse '.read_geom(file)' method to read a geometry from a file!\n")
+
+    #Initializing class Mol ...
     def __init__(self,geom_file=None):
         self.geom_file=geom_file
         self.__geom=None
         self.__bond_graph=None
+        self.__hbond_graph=None
+        self.__nhb=0
         if self.geom_file:
             self.read_geom(self.geom_file)
-        if self.__geom:
-            self.get_bond_graph(self.__geom)
 
-    # print coordinates to screen
+    # printing coordinates to screen
     def print_geom(self):
         if self.__geom:
             comment, at_types, coords = self.__geom[0:3]
             n_atoms = len(at_types)
-            print("Molecular geometry:\t'{}'".format(comment))
-            print("Number of atoms:\t{}".format(n_atoms))
+            print("Molecular geometry '{}':".format(comment))
+            print("Number of atoms - {}".format(n_atoms))
             for i in range(n_atoms):
                 print('%-2s' % (at_types[i]), end='')
                 for j in range(3):
@@ -100,14 +108,14 @@ class Mol:
                 print('\n', end='')
             print('\n', end='')
         else:
-            print("No geometry has been read in")
+            self.read_in_geom_err()
 
-    # print bond graph to screen
+    # printing bond graph to screen
     def print_bond_graph(self):
         if self.__bond_graph:
             comment, at_types, coords = self.__geom[0:3]
             n_atoms = len(at_types)
-            print("Bond graph:\t'{}'".format(comment))
+            print("Bond graph '{}':".format(comment))
             for i in range(n_atoms):
                 print("{:<2} {:<2}- ".format(i + 1, at_types[i]),end='')
                 for j in range(len(self.__bond_graph[i])):
@@ -115,10 +123,30 @@ class Mol:
                     if j < len(self.__bond_graph[i])-1:
                         print(', ',end='')
                     else:
-                        print()
-        else:
-            print("Bond graph has not yet been created!\n\rUse '{}.get_bond_graph' to creat it!".format
-                  (str(self.__class__).split('.')[0].split("'")[1]))
+                        print('\n', end='')
+            print('\n', end='')
 
+        else:
+            self.read_in_geom_err()
+
+    def print_hbond_graph(self):
+        if self.__hbond_graph:
+            if self.__nhb == 0:
+                print("No H-bonds have been identified!")
+                return
+            comment, at_types, coords = self.__geom[0:3]
+            n_atoms = len(at_types)
+            print("H-Bonds graph:")
+            for i in range(n_atoms):
+                for j in range(len(self.__hbond_graph[i])):
+                    if len(self.__hbond_graph[i] != 0):
+                        print("{:<2} {:<2}- ".format(i + 1, at_types[i]), end='')
+                        print("{}".format(self.__hbond_graph[i][j] + 1), end='')
+                        if j < len(self.__hbond_graph[i]) - 1:
+                            print(', ', end='')
+            else:
+                print()
+        else:
+            self.read_in_geom_err()
 
 ## Further
